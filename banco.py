b5c22537704859c1b61a4a1319bcb181d5fbbb68
João Gabriel Grossi Solis <104585766+joaogabgr@users.noreply.github.com>
2023-09-24 13:19:13 -0300

    Template do banco de dados atualizado

--- banco.py
+++ banco.py
@@ -14,3 +14,51 @@ except mysql.connector.Error as err:
             print('Existe algo errado no nome de usuário ou senha')
       else:
             print(err)
+
+cursor = conn.cursor()
+
+cursor.execute("DROP DATABASE IF EXISTS `API`;")
+
+cursor.execute("CREATE DATABASE `API`;")
+
+cursor.execute("USE `API`;")
+
+# criando tabelas
+TABLES = {}
+TABLES['Usuarios'] = ('''
+      CREATE TABLE `usuarios` (
+      email VARCHAR(255) PRIMARY KEY,
+      nome VARCHAR(255) not null,
+      senha VARCHAR(255) not null
+)''')
+
+TABLES['Posts'] = ('''
+      CREATE TABLE Posts (
+      id INT PRIMARY KEY auto_increment,
+      nome VARCHAR(255),
+      fk_email VARCHAR(255) not null,
+      transplante DATE,
+      nascimento DATE not null,
+      nome_filho VARCHAR(255) not null,
+      comentario LONGTEXT not null,
+      foreign key (fk_email) references usuarios(email)
+)''')
+
+for tabela_nome in TABLES:
+      tabela_sql = TABLES[tabela_nome]
+      try:
+            print('Criando tabela {}:'.format(tabela_nome), end=' ')
+            cursor.execute(tabela_sql)
+      except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
+                  print('Já existe')
+            else:
+                  print(err.msg)
+      else:
+            print('OK')
+
+# commitando se não nada tem efeito
+conn.commit()
+
+cursor.close()
+conn.close()
