be79503d92816d3c3594e663cad3e9adb6d1e587
João Gabriel Grossi Solis <104585766+joaogabgr@users.noreply.github.com>
2023-09-26 13:10:51 -0300

    Melhoria na estrutura dos dados

--- static/Js/script.js
+++ static/Js/script.js
@@ -1,3 +1,5 @@
+//  Abrir boxlogin
+
 let btnProfile = document.querySelector('.btnProfile')
 let popAccount = document.querySelector('.boxLogin')
 let arrow = document.querySelector('.arrow')
@@ -6,3 +8,17 @@ btnProfile.addEventListener('click', () => {
     popAccount.classList.toggle('ativo')
     arrow.classList.toggle('arrowativo')
 })
+
+// Comunidade -- mudar css do botao "Enviar fotos"
+
+let image = document.getElementById('image');
+let label = document.querySelector('.image');
+image.addEventListener('change', function() {
+    if (image.value != '') {
+        label.classList.add('com_foto')
+        label.innerHTML = 'Foto enviada'
+    } else {
+        label.classList.remove('com_foto')
+        label.innerHTML = 'Enviar fotos'
+    }
+});
