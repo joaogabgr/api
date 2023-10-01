06505e84ed26f0e2033db2456aa156acbbc80972
João Gabriel Grossi Solis <104585766+joaogabgr@users.noreply.github.com>
2023-10-01 05:55:09 -0300

    Foi adicionado responsividade ao site

--- static/Js/script.js
+++ static/Js/script.js
@@ -8,3 +8,22 @@ btnProfile.addEventListener('click', () => {
     popAccount.classList.toggle('ativo')
     arrow.classList.toggle('arrowativo')
 })
+
+// Abrir menuMobileBox
+
+let btnMenu = document.querySelector('.menuMobile')
+let menuMobileBox = document.querySelector('.menuMobileBox')
+
+btnMenu.addEventListener('click', () => {
+    if (menuMobileBox.classList.contains('ativo')) {
+        menuMobileBox.classList.remove('ativo')
+        menuMobileBox.classList.add('desativado')
+        btnMenu.classList.remove('ativo')
+        btnMenu.classList.add('desativado')
+    } else {
+        menuMobileBox.classList.remove('desativado')
+        menuMobileBox.classList.add('ativo')
+        btnMenu.classList.remove('desativado')
+        btnMenu.classList.add('ativo')
+    }
+})
